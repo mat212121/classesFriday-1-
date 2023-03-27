f117509c5374e8bac9be33d0349d42e180556643
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2023-03-27 22:00:09 +0000

    huh

--- attributes.py
+++ attributes.py
@@ -1,13 +1,23 @@
 # Create a class called House, and assign attributes to it: color, floors.
 
 # Create an instance of House, called white_house, with color "white" and number of floors equal to 4.
-
-
+class House:
+  def __init__(self,color,floors):
+    self.color = color
+    self.floors = floors 
+    floors = 4
+    color = 'white'
+  def white_hous(self,color,floors):
+    print(f'the house has {floors} floors and is {color}')
+    
 
 
 
 
 # Create a class called Cube, and assign the class attribute to it:
+class Cube: 
+  def __init__(self,slides,color):
+    self.slides = slides
 
 # sides = 6
 
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-class Pelican:
-  #attributes 
-  def __init__(self, wings, legs, neck, eyes, beak):
-    self.wings = wings
-    self.legs = legs
-    self.neck = neck
-    self.eyes = eyes
-    self.beak = beak
+# class Pelican:
+#   #attributes 
+#   def __init__(self, wings, legs, neck, eyes, beak):
+#     self.wings = wings
+#     self.legs = legs
+#     self.neck = neck
+#     self.eyes = eyes
+#     self.beak = beak
 
-  wings = 4
-  legs = 2
-  neck = 1
-  eyes = 2
-  def fly(self,wings):
-    print(f'birds can fly with {wings} wings')
+#   wings = 4
+#   legs = 2
+#   neck = 1
+#   eyes = 2
+#   def fly(self,wings):
+#     print(f'birds can fly with {wings} wings')
 
-  def canWalk(self):
-    print("birds can walk")
+#   def canWalk(self):
+#     print("birds can walk")
 
-  def canSwim(self):
-    print("birds can swim")
+#   def canSwim(self):
+#     print("birds can swim")
 
-wallie = Pelican(2,2,1,2,1)
-print(wallie.fly(2))
-print(wallie.canWalk())
+# wallie = Pelican(2,2,1,2,1)
+# print(wallie.fly(2))
+# print(wallie.canWalk())
+class House:
+  def __init__(self,color,floors):
+    self.color = color
+    self.floors = floors 
+    def white_house(self,color,floors):
+      print(f'the house has {floors} floors and is {color}')
+  floors = 4
+  color = 'white'
+  
+
+House = House('white',4)
+print(House)
